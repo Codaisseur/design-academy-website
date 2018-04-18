af607e69377d98d21661246857095606d6c3dea2
Wouter de Vos <wouter@codaisseur.com>
2018-04-18 12:08:07 +0200

    Link OEs to Design events only

--- src/data/open-evenings.js
+++ src/data/open-evenings.js
@@ -10,26 +10,29 @@ module.exports = async ({ boundActionCreators }) => {
       .get(`${API_URL}/open_evenings`)
       .then(res => {
         console.log('Fetched', res.body.open_evenings.length, 'open_evenings')
-        res.body.open_evenings.forEach(open_evening => {
-          createNode(
-            Object.assign(
-              {},
-              open_evening,
-              {
-                id: `open_evening-${open_evening.id.toString()}`,
-                parent: null,
-                children: [],
-                internal: {
-                  type: `OpenEvening`,
-                  contentDigest: crypto
-                    .createHash(`md5`)
-                    .update(`open_evening-${open_evening.id.toString()}`)
-                    .digest(`hex`),
+        res.body.open_evenings
+          .filter(oe => !!oe.design_event_url)
+          .forEach(open_evening => {
+            createNode(
+              Object.assign(
+                {},
+                open_evening,
+                {
+                  id: `open_evening-${open_evening.id.toString()}`,
+                  event_url: open_evening.design_event_url,
+                  parent: null,
+                  children: [],
+                  internal: {
+                    type: `OpenEvening`,
+                    contentDigest: crypto
+                      .createHash(`md5`)
+                      .update(`open_evening-${open_evening.id.toString()}`)
+                      .digest(`hex`),
+                  }
                 }
-              }
-            )
-          );
-        });
+              )
+            );
+          });
         resolve();
       })
       .catch(err => {
